c60220efff5a5e072f611af074ef28492210224a
NicolasDaniel21 <nicolasdanielw13@gmail.com>
2024-05-23 15:23:29 -0300

    Created the first graph, of the type "bar".

--- scripts/stock_chart.js
+++ scripts/stock_chart.js
@@ -1,8 +1,22 @@
-var stock = "PETR4.SA";
+var stock = "PETR4";
 
 /* Area to validate the form */
 
 var apiKey = "695FRTH84RDJMAGE";
-var urlDaily = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}&interval=5min&apikey=${apiKey}`;
+var urlDaily = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${stock}.SA&interval=5min&apikey=${apiKey}`;
 
 /* Area to do the search logic of the stocks */
+
+const ctx = document.getElementById('grafico1');
+
+new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1,
+        }]
+    }
+})
