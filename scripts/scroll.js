3a85a04d0dde9b98eaae81541ad060af1219c9ff
NicolasDaniel21 <nicolasdanielw13@gmail.com>
2024-10-07 00:36:32 -0300

    refactor: migration of files to the original project
    I replaced all the files with those developed in a separate workspace, where I finalized much of the project. I've kept the original project, but all the files have been updated to reflect the latest work.

---
+++ scripts/scroll.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href");
+      const targetElement = document.querySelector(targetId);
+      if (targetElement) {
+        const offset = 49;
+        const elementPosition = targetElement.getBoundingClientRect().top;
+        const offsetPosition = elementPosition + window.scrollY - offset;
+
+        window.scrollTo({
+          top: offsetPosition,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
